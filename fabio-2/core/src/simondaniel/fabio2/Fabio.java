7275420cbd67677de139e4622ea544380508e61e
Simon <simondaniel321@gmail.com>
2018-12-14 12:56:04 +0100

    fabio animation

--- fabio-2/core/src/simondaniel/fabio2/Fabio.java
+++ fabio-2/core/src/simondaniel/fabio2/Fabio.java
@@ -1,5 +1,7 @@
 package simondaniel.fabio2;
 
+import java.nio.file.Watchable;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -15,11 +17,12 @@ import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.utils.Array;
 
 import simondaniel.fabio2.gfx.AnimatedSprite;
+import simondaniel.fabio2.gfx.fabioAnimation;
 
 public class Fabio {
 	Body body;
 	
-	AnimatedSprite as;
+	fabioAnimation as;
 	
 	private Direction dir;
 	private State state;
@@ -30,14 +33,9 @@ public class Fabio {
 		state = State.STANDING;
 		
 		
-		TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("gfx/atlases/fabio/fabio2.atlas"));
 		
+		as = new fabioAnimation();
 		
-		as = new AnimatedSprite(atlas, new String[] {"hit", "walk", "crouch", "jump", "side"});
-		Array<AtlasRegion> region = atlas.getRegions();
-		for(AtlasRegion r : region) {
-			System.out.println("r: " + r.name);
-		}
 		
 		BodyDef bDef = new BodyDef();
 		bDef.position.x = 0;
@@ -55,26 +53,34 @@ public class Fabio {
 		setState(State.STANDING);
 	}
 	
+	State lastState;
+	
 	private void setState(State s) {
 		switch (s) {
 		case CROUCH:
-			as.runAnimation(2);
+			as.startCrouch();
 			break;
 		case HIT:
-			as.runAnimation(0);
+			as.performHit();
 			break;
 		case JUMP:
-			as.runOnce(3, false);
+			as.jumpUp();
+			break;
+		case FALL:
+			if(lastState != State.FALL)
+				as.fallDown();
 			break;
 		case STANDING:
-			as.runAnimation(4);
+			as.startIdle();
 			break;
 		case WALKING:
-			as.runAnimation(1);
+			if(lastState != State.WALKING)
+				as.startWalk();
 
 		default:
 			break;
 		}
+		lastState = s;
 	}
 	int i = 400;
 
@@ -82,7 +88,7 @@ public class Fabio {
 		as.update(delta);
 		i++;
 		
-		if(i > 200 && Gdx.input.isKeyJustPressed(Keys.W)) {
+		if(i > 100 && Gdx.input.isKeyJustPressed(Keys.W)) {
 			i = 0;
 		}
 		
@@ -92,21 +98,34 @@ public class Fabio {
 		
 		if(i == 14) {
 			body.setLinearVelocity(body.getLinearVelocity().x, 6);
-			
 		}
+		if( i == 80) {
+			setState(state.FALL);
+		}
+		boolean idle = true;
 		if(Gdx.input.isKeyPressed(Keys.A)) {
 			body.applyForceToCenter(new Vector2(-10,0), true);
 			dir = Direction.LEFT;
 			setState(State.WALKING);
+			idle = false;
 		}
 		if(Gdx.input.isKeyPressed(Keys.S)) {
 			body.applyForceToCenter(new Vector2(0,0), true);
 			setState(State.CROUCH);
+			idle = false;
 		}
 		if(Gdx.input.isKeyPressed(Keys.D)) {
 			body.applyForceToCenter(new Vector2(10,0), true);
 			dir = Direction.RIGHT;
 			setState(State.WALKING);
+			idle = false;
+		}
+		if(idle)
+			setState(state.STANDING);
+		
+		if(Gdx.input.isKeyJustPressed(Keys.SPACE)) {
+	
+			setState(State.HIT);
 		}
 		
 		as.setPosition(body.getPosition().cpy().add(0, 0.4f));
@@ -116,12 +135,12 @@ public class Fabio {
 	}
 	public void draw(SpriteBatch batch) {
 		as.setScale(1/MyGame.PPM);
-		as.setFlip(dir == Direction.LEFT, false);
+		as.setDirection(dir == Direction.LEFT);
 		as.draw(batch);
 	}
 	
 	private enum State{
-		WALKING, STANDING, HIT, JUMP, CROUCH;
+		WALKING, STANDING, HIT, JUMP, FALL, CROUCH;
 	}
 	
 	private enum Direction{
---
+++ fabio-2/core/src/simondaniel/fabio2/gfx/fabioAnimation.java
@@ -0,0 +1,195 @@
+package simondaniel.fabio2.gfx;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+public class fabioAnimation {
+	
+	private static final int HIT = 0, WALK = 1, CROUCH = 2, STAND_UP = 3, JUMP_UP = 4, JUMP_DOWN = 5, SIDE = 6;
+	
+	float elapsedTime;
+	int current;
+	Animation<TextureRegion>[] animations;
+
+	private boolean halted;
+	private int haltAt = -1;
+	
+	Sprite sprite;
+	
+	boolean isIdle;
+
+	@SuppressWarnings("unchecked")
+	public fabioAnimation() {
+		
+		sprite = new Sprite();
+		
+		this.animations = new Animation[7];
+		
+		String[] animNames = new String[] {"hit", "walk", "crouch", "jump", "side"};
+		
+		TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("gfx/atlases/fabio/fabio2.atlas"));
+		
+		Array<AtlasRegion> region = atlas.getRegions();
+		for(AtlasRegion r : region) {
+			System.out.println("r: " + r.name);
+		}
+		
+		Animation<TextureRegion> animation = new Animation<TextureRegion>(1f / 3f, atlas.findRegions("hit"));
+		animation.setPlayMode(PlayMode.LOOP);
+		animation.setFrameDuration(0.14f);
+		this.animations[HIT] = animation;
+		
+		animation = new Animation<TextureRegion>(1f / 3f, atlas.findRegions("walk"));
+		animation.setPlayMode(PlayMode.LOOP);
+		animation.setFrameDuration(0.2f);
+		this.animations[WALK] = animation;
+		
+		animation = new Animation<TextureRegion>(1f / 3f, atlas.findRegions("crouch"));
+		animation.setPlayMode(PlayMode.NORMAL);
+		animation.setFrameDuration(0.12f);
+		this.animations[CROUCH] = animation;
+		
+		animation = new Animation<TextureRegion>(1f / 3f, atlas.findRegions("crouch"));
+		animation.setPlayMode(PlayMode.REVERSED);
+		animation.setFrameDuration(0.12f);
+		this.animations[STAND_UP] = animation;
+		
+		animation = new Animation<TextureRegion>(1f / 3f, atlas.findRegions("jump"));
+		animation.setPlayMode(PlayMode.NORMAL);
+		animation.setFrameDuration(0.09f);
+		this.animations[JUMP_UP] = animation;
+		
+		animation = new Animation<TextureRegion>(1f / 3f, atlas.findRegions("jump"));
+		animation.setPlayMode(PlayMode.REVERSED);
+		animation.setFrameDuration(0.09f);
+		this.animations[JUMP_DOWN] = animation;
+		
+		animation = new Animation<TextureRegion>(1f / 3f, atlas.findRegions("side"));
+		animation.setPlayMode(PlayMode.LOOP);
+		animation.setFrameDuration(0.15f);
+		this.animations[SIDE] = animation;
+
+		current = SIDE;
+		Animation<TextureRegion> currentAnimation = animations[current];
+		sprite.setRegion(currentAnimation.getKeyFrames()[0]);
+		sprite.setSize(currentAnimation.getKeyFrames()[0].getRegionWidth(), currentAnimation.getKeyFrames()[0].getRegionHeight());
+		sprite.setOriginCenter();
+	}
+
+	public void update(float delta) {
+		if (!halted) {
+			elapsedTime += delta;
+			
+			TextureRegion frame = animations[current].getKeyFrame(elapsedTime);
+			sprite.setRegion(frame);
+			switch (current) {
+			case JUMP_DOWN:
+				if(animations[current].isAnimationFinished(elapsedTime)) {
+					startIdle();
+					
+				}
+				
+				break;
+
+			default:
+				break;
+			}
+			//setSize(getRegionWidth()*1.0f, getRegionHeight()*0.65f);
+			//setOriginCenter();
+		
+			if (animations[current].getKeyFrameIndex(elapsedTime) == haltAt) {
+				halted = true;
+			}
+		}
+
+	}
+
+	public void setPosition(float x, float y) {
+		sprite.setPosition(x - sprite.getOriginX(), y - sprite.getOriginY());
+	}
+	
+	public void setPosition(Vector2 position) {
+		setPosition(position.x, position.y);
+	}
+	
+	public void drawCentered(SpriteBatch sb) {
+		sprite.draw(sb);
+	}
+	
+	public void startWalk() {
+		if(current == JUMP_UP || current == JUMP_DOWN) return;
+		elapsedTime = 0;
+		current = WALK;
+	}
+
+	public void startCrouch() {
+		elapsedTime = 0;
+		current = CROUCH;
+	}
+	
+	public void jumpUp() {
+		elapsedTime = 0;
+		current = JUMP_UP;
+	}
+	
+	public void fallDown() {
+		elapsedTime = 0;
+		current = JUMP_DOWN;
+	}
+	
+	public void performHit() {
+		elapsedTime = 0;
+		current = HIT;
+	}
+	
+	public void startIdle() {
+		isIdle = true;
+	}
+	
+	public void runOnce(int i, boolean loop) {
+		if (i < 0 || i >= animations.length)
+			return;
+		elapsedTime = 0;
+		haltAt = -1;
+		halted = false;
+		current =  i;
+		animations[i].setPlayMode(loop? PlayMode.LOOP : PlayMode.NORMAL);
+	}
+
+	public void haltAnimation() {
+		halted = true;
+	}
+
+	public void haltAnimation(int frame) {
+		haltAt = frame;
+	}
+
+	public void drawOutline(ShapeRenderer sr) {
+		
+		sr.rect(sprite.getX(), sprite.getY(), sprite.getWidth(), sprite.getHeight());
+	}
+
+	public void setDirection(boolean isLeft) {
+		sprite.setFlip(isLeft, false);
+	}
+
+	public void setScale(float f) {
+		sprite.setScale(f);
+	}
+
+	public void draw(SpriteBatch batch) {
+		sprite.draw(batch);
+	}
+
+
+	
+}
